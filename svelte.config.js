72ae1522b59e1cf44fb68a895589abb085719d12
SAMEER TARIQ <imsamtar@gmail.com>
2021-08-02 22:41:34 +0500

    update port

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,11 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: node()
+		adapter: node({
+			env: {
+				port: 3001
+			}
+		})
 	}
 };
 
