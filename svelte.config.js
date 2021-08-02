afaa22b335c3a8032535f86058a6ee4133dac0aa
SAMEER TARIQ <imsamtar@gmail.com>
2021-08-02 23:31:20 +0500

    update

--- svelte.config.js
+++ svelte.config.js
@@ -12,11 +12,7 @@ const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: node({
-			env: {
-				port: 3001
-			}
-		})
+		adapter: node()
 	}
 };
 
